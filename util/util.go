10b73690569d289e6b48f6ef66cd60f47fbeb97a
Meghan <nektromc@gmail.com>
2019-02-28 13:03:45 -0500

    util - add handleFunc like calls for fasthttp usage

--- util/util.go
+++ util/util.go
@@ -3,6 +3,12 @@ package util
 import (
 	"fmt"
 	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	fasthttpHandlers = map[string]func(ctx *fasthttp.RequestCtx){}
 )
 
 func Log(message ...interface{}) {
@@ -13,4 +19,18 @@ func Log(message ...interface{}) {
 func GetIsoDateTime() string {
 	vil := time.Now().UTC().String()
 	return vil[0:19]
+}
+
+func FasthttpAddHandler(path string, handle func(ctx *fasthttp.RequestCtx)) {
+	fasthttpHandlers[path] = handle
+}
+
+func FasthttpHandle(path string, ctx *fasthttp.RequestCtx) bool {
+	for k, v := range fasthttpHandlers {
+		if k == path {
+			v(ctx)
+			return true
+		}
+	}
+	return false
 }
