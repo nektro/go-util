5f35218388c3ada83012495021d585213b1d28e5
Meghan <nektromc@gmail.com>
2019-06-02 04:45:42 -0400

    alias - add E for errors.New

--- alias/alias.go
+++ alias/alias.go
@@ -1,9 +1,14 @@
 package alias
 
 import (
+	"errors"
 	"fmt"
 )
 
 func F(base string, args ...interface{}) string {
 	return fmt.Sprintf(base, args...)
+}
+
+func E(message string) error {
+	return errors.New(message)
 }
