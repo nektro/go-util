a972d35088fd47c7875fdf8220958dd7cc486de2
Meghan Denny <nektromc@gmail.com>
2020-06-29 04:01:21 -0400

    util: internal hash map now stores new function instead

--- util/hash.go
+++ util/hash.go
@@ -3,6 +3,7 @@ package util
 import (
 	"crypto"
 	"encoding/hex"
+	"hash"
 	"io"
 
 	// ensure crypto algorithms are initialized
@@ -19,29 +20,29 @@ import (
 )
 
 var (
-	algoMap = map[string]crypto.Hash{}
+	algoMap = map[string]func() hash.Hash{}
 )
 
 func init() {
-	algoMap["MD4"] = crypto.MD4
-	algoMap["MD5"] = crypto.MD5
-	algoMap["SHA1"] = crypto.SHA1
-	algoMap["SHA224"] = crypto.SHA224
-	algoMap["SHA256"] = crypto.SHA256
-	algoMap["SHA384"] = crypto.SHA384
-	algoMap["SHA512"] = crypto.SHA512
-	algoMap["MD5SHA1"] = crypto.MD5SHA1
-	algoMap["RIPEMD160"] = crypto.RIPEMD160
-	algoMap["SHA3_224"] = crypto.SHA3_224
-	algoMap["SHA3_256"] = crypto.SHA3_256
-	algoMap["SHA3_384"] = crypto.SHA3_384
-	algoMap["SHA3_512"] = crypto.SHA3_512
-	algoMap["SHA512_224"] = crypto.SHA512_224
-	algoMap["SHA512_256"] = crypto.SHA512_256
-	algoMap["BLAKE2s_256"] = crypto.BLAKE2s_256
-	algoMap["BLAKE2b_256"] = crypto.BLAKE2b_256
-	algoMap["BLAKE2b_384"] = crypto.BLAKE2b_384
-	algoMap["BLAKE2b_512"] = crypto.BLAKE2b_512
+	algoMap["MD4"] = crypto.MD4.New
+	algoMap["MD5"] = crypto.MD5.New
+	algoMap["SHA1"] = crypto.SHA1.New
+	algoMap["SHA224"] = crypto.SHA224.New
+	algoMap["SHA256"] = crypto.SHA256.New
+	algoMap["SHA384"] = crypto.SHA384.New
+	algoMap["SHA512"] = crypto.SHA512.New
+	algoMap["MD5SHA1"] = crypto.MD5SHA1.New
+	algoMap["RIPEMD160"] = crypto.RIPEMD160.New
+	algoMap["SHA3_224"] = crypto.SHA3_224.New
+	algoMap["SHA3_256"] = crypto.SHA3_256.New
+	algoMap["SHA3_384"] = crypto.SHA3_384.New
+	algoMap["SHA3_512"] = crypto.SHA3_512.New
+	algoMap["SHA512_224"] = crypto.SHA512_224.New
+	algoMap["SHA512_256"] = crypto.SHA512_256.New
+	algoMap["BLAKE2s_256"] = crypto.BLAKE2s_256.New
+	algoMap["BLAKE2b_256"] = crypto.BLAKE2b_256.New
+	algoMap["BLAKE2b_384"] = crypto.BLAKE2b_384.New
+	algoMap["BLAKE2b_512"] = crypto.BLAKE2b_512.New
 }
 
 func Hash(algo string, bys []byte) string {
@@ -49,7 +50,7 @@ func Hash(algo string, bys []byte) string {
 	if !ok {
 		return ""
 	}
-	h := c.New()
+	h := c()
 	h.Write(bys)
 	return hex.EncodeToString(h.Sum([]byte{}))
 }
@@ -59,7 +60,7 @@ func HashStream(algo string, rc io.ReadCloser) string {
 	if !ok {
 		return ""
 	}
-	h := c.New()
+	h := c()
 	io.Copy(h, rc)
 	return hex.EncodeToString(h.Sum([]byte{}))
 }
